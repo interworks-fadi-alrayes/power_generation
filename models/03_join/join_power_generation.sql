0a4258a5e43d084506b42ed7393f9bd2248cc93b
fadi_alrayes <fadi.alrayes@interworks.de>
2025-11-16 15:08:10 +0000

    Linted all models

--- models/03_join/join_power_generation.sql
+++ models/03_join/join_power_generation.sql
@@ -1,167 +1,173 @@
 with fact as (
-    select * from {{ ref('clean_fact_power_generation') }}
+  select
+    *
+  from {{ ref('clean_fact_power_generation') }}
 ),
 
 units as (
-    select * from {{ ref('clean_dim_unit') }}
+  select
+    *
+  from {{ ref('clean_dim_unit') }}
 ),
 
 weather as (
-    select * from {{ ref('clean_dim_weather') }}
+  select
+    *
+  from {{ ref('clean_dim_weather') }}
 ),
 
 fact_parsed as (
-    select
-        power_generation_id,
-        power_generation_unit_id,
-        value_mw,
+  select
+    power_generation_id,
+    power_generation_unit_id,
+    value_mw,
 
-        -- Parse start_date from DD.MM.YYYY HH:MM format
-        start_date,
-        end_date,
+    -- Parse start_date from DD.MM.YYYY HH:MM format
+    start_date,
+    end_date,
 
-        -- Parse end_date from DD.MM.YYYY HH:MM format
-        to_timestamp(start_date, 'DD.MM.YYYY HH24:MI') as start_datetime,
-        to_timestamp(end_date, 'DD.MM.YYYY HH24:MI') as end_datetime,
+    -- Parse end_date from DD.MM.YYYY HH:MM format
+    to_timestamp(start_date, 'DD.MM.YYYY HH24:MI') as start_datetime,
+    to_timestamp(end_date, 'DD.MM.YYYY HH24:MI') as end_datetime,
 
-        -- Extract date components from start_date
-        year(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_year,
-        month(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_month,
-        day(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_day,
-        hour(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_hour,
-        minute(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_minute,
-        to_date(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI'))
-            as start_date_only,
+    -- Extract date components from start_date
+    year(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_year,
+    month(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_month,
+    day(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_day,
+    hour(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_hour,
+    minute(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI')) as start_minute,
+    to_date(to_timestamp(start_date, 'DD.MM.YYYY HH24:MI'))
+      as start_date_only,
 
-        -- Extract date components from end_date
-        year(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_year,
-        month(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_month,
-        day(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_day,
-        hour(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_hour,
-        minute(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_minute,
+    -- Extract date components from end_date
+    year(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_year,
+    month(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_month,
+    day(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_day,
+    hour(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_hour,
+    minute(to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')) as end_minute,
 
-        -- Calculate duration in hours
-        datediff(
-            hour,
-            to_timestamp(start_date, 'DD.MM.YYYY HH24:MI'),
-            to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')
-        ) as duration_hours
+    -- Calculate duration in hours
+    datediff(
+      hour,
+      to_timestamp(start_date, 'DD.MM.YYYY HH24:MI'),
+      to_timestamp(end_date, 'DD.MM.YYYY HH24:MI')
+    ) as duration_hours
 
-    from fact
+  from fact
 ),
 
 weather_parsed as (
-    select
-        postal_code,
-        time,
-        temperature_2m_c,
+  select
+    postal_code,
+    time,
+    temperature_2m_c,
 
-        -- Extract date components
-        wind_speed_10m_m_s,
-        wind_speed_80m_m_s,
-        shortwave_radiation_w_m2,
-        direct_radiation_w_m2,
-        diffuse_radiation_w_m2,
+    -- Extract date components
+    wind_speed_10m_m_s,
+    wind_speed_80m_m_s,
+    shortwave_radiation_w_m2,
+    direct_radiation_w_m2,
+    diffuse_radiation_w_m2,
 
-        -- Weather measurements
-        precipitation_mm,
-        cloud_cover,
-        pressure_msl_hpa,
-        rain_mm,
-        snowfall_cm,
-        country_english,
-        latitude,
-        longitude,
-        to_timestamp(time) as weather_datetime,
-        year(to_timestamp(time)) as weather_year,
-        month(to_timestamp(time)) as weather_month,
-        day(to_timestamp(time)) as weather_day,
-        hour(to_timestamp(time)) as weather_hour,
-        to_date(to_timestamp(time)) as weather_date_only
+    -- Weather measurements
+    precipitation_mm,
+    cloud_cover,
+    pressure_msl_hpa,
+    rain_mm,
+    snowfall_cm,
+    country_english,
+    latitude,
+    longitude,
+    to_timestamp(time) as weather_datetime,
+    year(to_timestamp(time)) as weather_year,
+    month(to_timestamp(time)) as weather_month,
+    day(to_timestamp(time)) as weather_day,
+    hour(to_timestamp(time)) as weather_hour,
+    to_date(to_timestamp(time)) as weather_date_only
 
-    from weather
+  from weather
 )
 
 select
-    -- Fact identifiers
-    fp.power_generation_id,
-    fp.power_generation_unit_id,
+  -- Fact identifiers
+  fp.power_generation_id,
+  fp.power_generation_unit_id,
 
-    -- Unit information
-    u.unit_name,
-    u.operator,
-    u.unit_street,
-    u.unit_house_number,
-    u.unit_postal_code,
-    u.unit_city,
-    u.unit_state,
-    u.energy_source,
-    u.is_storage,
-    u.voltage_level,
-    u.plant_status,
-    u.commissioning_year,
-    u.decommissioning_year,
-    u.gross_capacity_mw,
-    u.net_electrical_capacity_mw,
-    u.is_cross_border_unit,
-    u.feed_in_type,
-    u.grid_operator,
+  -- Unit information
+  u.unit_name,
+  u.operator,
+  u.unit_street,
+  u.unit_house_number,
+  u.unit_postal_code,
+  u.unit_city,
+  u.unit_state,
+  u.energy_source,
+  u.is_storage,
+  u.voltage_level,
+  u.plant_status,
+  u.commissioning_year,
+  u.decommissioning_year,
+  u.gross_capacity_mw,
+  u.net_electrical_capacity_mw,
+  u.is_cross_border_unit,
+  u.feed_in_type,
+  u.grid_operator,
 
-    -- Power generation temporal data
-    fp.start_date,
-    fp.start_datetime,
-    fp.start_year,
-    fp.start_month,
-    fp.start_day,
-    fp.start_hour,
-    fp.start_minute,
-    fp.start_date_only,
+  -- Power generation temporal data
+  fp.start_date,
+  fp.start_datetime,
+  fp.start_year,
+  fp.start_month,
+  fp.start_day,
+  fp.start_hour,
+  fp.start_minute,
+  fp.start_date_only,
 
-    fp.end_date,
-    fp.end_datetime,
-    fp.end_year,
-    fp.end_month,
-    fp.end_day,
-    fp.end_hour,
-    fp.end_minute,
+  fp.end_date,
+  fp.end_datetime,
+  fp.end_year,
+  fp.end_month,
+  fp.end_day,
+  fp.end_hour,
+  fp.end_minute,
 
-    fp.duration_hours,
+  fp.duration_hours,
 
-    -- Power generation measurements
-    fp.value_mw,
+  -- Power generation measurements
+  fp.value_mw,
 
-    -- Calculated metrics
-    w.weather_datetime,
-    w.weather_year,
+  -- Calculated metrics
+  w.weather_datetime,
+  w.weather_year,
 
-    -- Weather data
-    w.weather_month,
-    w.weather_day,
-    w.weather_hour,
-    w.temperature_2m_c,
-    w.wind_speed_10m_m_s,
-    w.wind_speed_80m_m_s,
-    w.shortwave_radiation_w_m2,
-    w.direct_radiation_w_m2,
-    w.diffuse_radiation_w_m2,
-    w.precipitation_mm,
-    w.cloud_cover,
-    w.pressure_msl_hpa,
-    w.rain_mm,
-    w.snowfall_cm,
-    w.country_english,
-    w.latitude,
-    w.longitude,
-    fp.value_mw * fp.duration_hours as energy_mwh,
-    case
-        when u.net_electrical_capacity_mw > 0
-            then (fp.value_mw / u.net_electrical_capacity_mw) * 100
-    end as capacity_utilization_pct
+  -- Weather data
+  w.weather_month,
+  w.weather_day,
+  w.weather_hour,
+  w.temperature_2m_c,
+  w.wind_speed_10m_m_s,
+  w.wind_speed_80m_m_s,
+  w.shortwave_radiation_w_m2,
+  w.direct_radiation_w_m2,
+  w.diffuse_radiation_w_m2,
+  w.precipitation_mm,
+  w.cloud_cover,
+  w.pressure_msl_hpa,
+  w.rain_mm,
+  w.snowfall_cm,
+  w.country_english,
+  w.latitude,
+  w.longitude,
+  fp.value_mw * fp.duration_hours as energy_mwh,
+  case
+    when u.net_electrical_capacity_mw > 0
+      then (fp.value_mw / u.net_electrical_capacity_mw) * 100
+  end as capacity_utilization_pct
 
 from fact_parsed as fp
 left join units as u
-    on fp.power_generation_unit_id = u.power_generation_unit_id
+  on fp.power_generation_unit_id = u.power_generation_unit_id
 left join weather_parsed as w
-    on
-        u.unit_postal_code = w.postal_code
-        and fp.start_datetime = w.weather_datetime
+  on
+    u.unit_postal_code = w.postal_code
+    and fp.start_datetime = w.weather_datetime
--- models/04_aggregate/aggregate_power_generation_daily.sql
+++ models/04_aggregate/aggregate_power_generation_daily.sql
@@ -1,84 +1,86 @@
 with joined as (
-    select * from {{ ref('join_power_generation') }}
+  select
+    *
+  from {{ ref('join_power_generation') }}
 )
 
 select
-    -- Date dimension
-    start_date_only as generation_date,
-    start_year,
-    start_month,
-    start_day,
+  -- Date dimension
+  start_date_only as generation_date,
+  start_year,
+  start_month,
+  start_day,
 
-    -- Unit dimension
-    power_generation_unit_id,
-    unit_name,
-    operator,
-    unit_city,
-    unit_state,
-    unit_postal_code,
-    energy_source,
-    plant_status,
-    voltage_level,
-    is_storage,
+  -- Unit dimension
+  power_generation_unit_id,
+  unit_name,
+  operator,
+  unit_city,
+  unit_state,
+  unit_postal_code,
+  energy_source,
+  plant_status,
+  voltage_level,
+  is_storage,
 
-    -- Unit capacity (takes latest value per day)
-    max(gross_capacity_mw) as gross_capacity_mw,
-    max(net_electrical_capacity_mw) as net_electrical_capacity_mw,
+  -- Unit capacity (takes latest value per day)
+  max(gross_capacity_mw) as gross_capacity_mw,
+  max(net_electrical_capacity_mw) as net_electrical_capacity_mw,
 
-    -- Power generation aggregations
-    count(*) as number_of_readings,
-    sum(duration_hours) as total_duration_hours,
-    sum(energy_mwh) as total_energy_mwh,
-    avg(value_mw) as avg_power_mw,
-    min(value_mw) as min_power_mw,
-    max(value_mw) as max_power_mw,
-    stddev(value_mw) as stddev_power_mw,
+  -- Power generation aggregations
+  count(*) as number_of_readings,
+  sum(duration_hours) as total_duration_hours,
+  sum(energy_mwh) as total_energy_mwh,
+  avg(value_mw) as avg_power_mw,
+  min(value_mw) as min_power_mw,
+  max(value_mw) as max_power_mw,
+  stddev(value_mw) as stddev_power_mw,
 
-    -- Capacity utilization
-    avg(capacity_utilization_pct) as avg_capacity_utilization_pct,
-    min(capacity_utilization_pct) as min_capacity_utilization_pct,
-    max(capacity_utilization_pct) as max_capacity_utilization_pct,
+  -- Capacity utilization
+  avg(capacity_utilization_pct) as avg_capacity_utilization_pct,
+  min(capacity_utilization_pct) as min_capacity_utilization_pct,
+  max(capacity_utilization_pct) as max_capacity_utilization_pct,
 
-    -- Weather aggregations (daily averages)
-    avg(temperature_2m_c) as avg_temperature_2m_c,
-    min(temperature_2m_c) as min_temperature_2m_c,
-    max(temperature_2m_c) as max_temperature_2m_c,
+  -- Weather aggregations (daily averages)
+  avg(temperature_2m_c) as avg_temperature_2m_c,
+  min(temperature_2m_c) as min_temperature_2m_c,
+  max(temperature_2m_c) as max_temperature_2m_c,
 
-    avg(wind_speed_10m_m_s) as avg_wind_speed_10m_m_s,
-    max(wind_speed_10m_m_s) as max_wind_speed_10m_m_s,
+  avg(wind_speed_10m_m_s) as avg_wind_speed_10m_m_s,
+  max(wind_speed_10m_m_s) as max_wind_speed_10m_m_s,
 
-    avg(wind_speed_80m_m_s) as avg_wind_speed_80m_m_s,
-    max(wind_speed_80m_m_s) as max_wind_speed_80m_m_s,
+  avg(wind_speed_80m_m_s) as avg_wind_speed_80m_m_s,
+  max(wind_speed_80m_m_s) as max_wind_speed_80m_m_s,
 
-    avg(shortwave_radiation_w_m2) as avg_shortwave_radiation_w_m2,
-    avg(direct_radiation_w_m2) as avg_direct_radiation_w_m2,
-    avg(diffuse_radiation_w_m2) as avg_diffuse_radiation_w_m2,
+  avg(shortwave_radiation_w_m2) as avg_shortwave_radiation_w_m2,
+  avg(direct_radiation_w_m2) as avg_direct_radiation_w_m2,
+  avg(diffuse_radiation_w_m2) as avg_diffuse_radiation_w_m2,
 
-    sum(precipitation_mm) as total_precipitation_mm,
-    sum(rain_mm) as total_rain_mm,
-    sum(snowfall_cm) as total_snowfall_cm,
+  sum(precipitation_mm) as total_precipitation_mm,
+  sum(rain_mm) as total_rain_mm,
+  sum(snowfall_cm) as total_snowfall_cm,
 
-    avg(cloud_cover) as avg_cloud_cover,
-    avg(pressure_msl_hpa) as avg_pressure_msl_hpa,
+  avg(cloud_cover) as avg_cloud_cover,
+  avg(pressure_msl_hpa) as avg_pressure_msl_hpa,
 
-    -- Location
-    max(country_english) as country,
-    max(latitude) as latitude,
-    max(longitude) as longitude
+  -- Location
+  max(country_english) as country,
+  max(latitude) as latitude,
+  max(longitude) as longitude
 
 from joined
 group by
-    start_date_only,
-    start_year,
-    start_month,
-    start_day,
-    power_generation_unit_id,
-    unit_name,
-    operator,
-    unit_city,
-    unit_state,
-    unit_postal_code,
-    energy_source,
-    plant_status,
-    voltage_level,
-    is_storage
+  start_date_only,
+  start_year,
+  start_month,
+  start_day,
+  power_generation_unit_id,
+  unit_name,
+  operator,
+  unit_city,
+  unit_state,
+  unit_postal_code,
+  energy_source,
+  plant_status,
+  voltage_level,
+  is_storage
