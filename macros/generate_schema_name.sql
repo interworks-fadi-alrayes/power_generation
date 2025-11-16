b8e0a8bb0c6af2b351e511908a48dc639909c87d
fadi_alrayes <fadi.alrayes@interworks.de>
2025-11-16 17:01:49 +0000

    test ci

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,20 +2,17 @@
   {%- set re = modules.re -%}
   {%- set default_schema = target.schema -%}
   {% set target_name = target.name %}
+    {% set pr_number = ('pr' ~ env_var('DBT_CLOUD_PR_ID', '')) if env_var('DBT_CLOUD_PR_ID', '') else env_var('DBT_PR_NUMBER', 'none') %}
+
 
   {% if target_name == 'ci' %}
-    {# CI: Use dbt's default behavior #}
-    {%- if custom_schema_name is none -%}
-      {{ default_schema }}
-    {%- else -%}
-      {{ default_schema }}_{{ custom_schema_name | trim }}
-    {%- endif -%}
+    {{ custom_schema_name }}_{{ pr_number | lower | trim }}
 
   {% elif target_name == 'prod' %}
     {# Prod: Just use custom schema name as-is #}
     {{ custom_schema_name }}
 
-  {% elif target_name == 'dev' %}
+  {% elif target_name in ['dev', 'default'] %}
     {# Dev: Custom schema + branch name suffix #}
     {% set branch = env_var('DBT_CLOUD_GIT_BRANCH', env_var('DBT_CURRENT_BRANCH_NAME', '')) %}
 
