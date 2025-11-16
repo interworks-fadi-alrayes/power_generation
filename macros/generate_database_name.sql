10dd741dd965bd680b487e7a517e74f5abc0eff0
fadi-alrayes_biontech <fadi.alrayes@external.biontech.com>
2025-11-16 17:51:31 +0100

    Configure database and schema naming for dev, ci, and prod environments
    Database naming:
    - prod: power_generation_prod
    - ci: power_generation_ci
    - dev: power_generation_dev
    
    Schema naming:
    - prod: Use custom schema name only (e.g., O2_CLEAN)
    - dev: Append branch name suffix (e.g., O2_CLEAN_main)
    - ci: Use dbt default behavior

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,7 +1,10 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
     {%- set default_database = target.database -%}
 
-    {% if target.name == 'prod' %}
+    {# Use custom database if provided from dbt_project.yml #}
+    {% if custom_database_name %}
+        {{ custom_database_name }}
+    {% elif target.name == 'prod' %}
         {{ 'power_generation_prod' }}
     {% elif target.name == 'ci' %}
         {{ 'power_generation_ci' }}
