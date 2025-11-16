ca81c118be0d3a5cb608f1471492ed02b9ade483
fadi-alrayes_biontech <fadi.alrayes@external.biontech.com>
2025-11-16 17:32:22 +0100

    modify generate database name

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,20 +1,18 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
     {%- set default_database = target.database -%}
-    {%- set base_database = custom_database_name | trim | lower -%}
-    {%- set base_project = project_name -%}
 
-    {# Use dev suffix for non-prod environments #}
-    {% if target.name == 'prod' %}
-        {%- set generated_database = 'power_generation_prod' -%}
+    {# Use custom database if provided, otherwise use target.database #}
+    {% if custom_database_name %}
+        {{ custom_database_name }}
+    {% elif target.name == 'prod' %}
+        {{ 'power_generation_prod' }}
     {% elif target.name == 'ci' %}
-        {%- set generated_database = 'power_generation_ci' -%}
-    {%- else -%}
-        {%- set generated_database = 'power_generation_dev' -%}
-    {%- endif -%}
+        {{ 'power_generation_ci' }}
+    {% else %}
+        {# For dev, use the existing database from target #}
+        {{ default_database }}
+    {% endif %}
 
-    {{ generated_database }}
-
- 
 {%- endmacro %}
 
 
