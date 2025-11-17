a0ca2a925bd3cc7d1b80e0f46cbdbffb0ec7526d
fadi-alrayes_biontech <fadi.alrayes@external.biontech.com>
2025-11-17 14:00:45 +0100

    Add MetricFlow time spine model for semantic layer
    Created time spine model required by dbt semantic layer:
    - Daily granularity from 2023-01-01 to 2026-12-31
    - Materialized as table in O0_UTILITY schema
    - Used by MetricFlow for time-based metric calculations
    
    Files added:
    - models/00_time_spine/metricflow_time_spine.sql
    - models/00_time_spine/metricflow_time_spine.yml
    
    Updated dbt_project.yml to configure time spine schema

---
+++ models/00_time_spine/metricflow_time_spine.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with date_spine as (
+    {{ dbt.date_spine(
+        datepart="day",
+        start_date="cast('2023-01-01' as date)",
+        end_date="cast('2026-12-31' as date)"
+    ) }}
+)
+
+select
+    date_day as date_day
+from date_spine
